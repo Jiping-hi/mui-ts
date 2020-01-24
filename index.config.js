ac241b30bd05d5c7b41a967eee2917701e27cb5c
Jiping Wang <jwang@healthinteractiveinc.com>
2020-01-24 09:34:31 -0500

    SystemJS-config-map-packages
    map vs package main

--- index.config.js
+++ index.config.js
@@ -1,21 +1,18 @@
 /* global React, ReactDOM, MaterialUI */
-
-
 //SystemJS settings
 SystemJS.config({
-  //defaultJSExtensions: 'js',
   paths: {
     api: '/api',
     src: '/src',
     node_modules: '/node_modules',
     'npm:': '/node_modules/',
-    '@material-ui/icons/*': '/node_modules/@material-ui/icons/*',
+    // '@material-ui/icons/*': '/node_modules/@material-ui/icons/*',
   },
 
   packages: {
     '.': { defaultExtension: 'js' },
     //'typescript': {  main: 'typescript.min.js', meta: { '*': { exports: 'ts' } } }, //getlibs original
-    typescript: { main: 'typescript.js', meta: { '*': { exports: 'ts' } } }, //unpkg.com typescript no min
+    typescript: { meta: { '*': { exports: 'ts' } } }, //unpkg.com typescript no min
     api: {
       defaultExtension: 'tsx'
     },
@@ -25,16 +22,6 @@ SystemJS.config({
     'src/anychart-react': {
       defaultExtension: ''
     },
-
-    // '@material-ui/core/SvgIcon': {
-
-    // },
-
-
-    // 'css-properties': {
-    //   defaultExtension: '',
-    //   main: 'index.js',
-    // },
 
     'ag-grid-community': {
       "main": "./dist/ag-grid-community.cjs.js",
@@ -46,22 +33,11 @@ SystemJS.config({
     //'typescript': 'https://unpkg.com/typescript@3.7.2/lib/typescript.js',
     //'typescript': 'https://cdnjs.cloudflare.com/ajax/libs/typescript/2.6.2', //getlibs original
     // typescript: 'https://cdnjs.cloudflare.com/ajax/libs/typescript/3.7.2',
-    typescript: '/node_modules/typescript/lib',
+    typescript: 'npm:typescript/lib/typescript.js',
     // UMD packages
-    // react: '/node_modules/react/umd/react.development.js',
-    // 'react-dom': '/node_modules/react-dom/umd/react-dom.development.js',
-    // 'react-router-dom': '/node_modules/react-router-dom/umd/react-router-dom.js',
     'react-is': '/node_modules/react-is/umd/react-is.development.js',
     'prop-types': 'npm:prop-types/prop-types.js',
 
-    // '@material-ui/core': 'npm:@material-ui/core/umd/material-ui.development.js',
-    // '@material-ui/core/SvgIcon': 'npm:@material-ui/core/umd/material-ui.development.js',
-    // '@material-ui/icons': 'npm:@material-ui/icons',
-
-    'anychart': 'npm:anychart/dist/js/anychart-bundle.min.js',
-    'anychart-react': '/src/anychart-react/src/anychart-react.jsx',
-    // 'capitalize': 'npm:capitalize',
-    // 'ag-grid-react': 'npm:ag-grid-react', //by umd
     'ag-grid-react': 'npm:ag-grid-react/umd/ag-grid-react.min.js',
     'ag-grid-community': 'npm:ag-grid-community',
     'ag-grid-enterprise': 'npm:ag-grid-enterprise',
@@ -77,17 +53,11 @@ SystemJS.config({
   bundles: {},
 });
 
-const process = {
-  env: {
-    NODE_ENV: 'production'
-  }
-}
 
 // globals
 let externals = {
   "react": { default: React },
   "react-dom": ReactDOM,
-  // '@material-ui/core/SvgIcon': MaterialUI.SvgIcon,
   '@material-ui/core': MaterialUI,
 };
 for (const m in externals) {
